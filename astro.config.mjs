56b9ad5f637785a2ea4c44dd4ff30bc2bb6faf9a
mahan.mattr <mahan.mattr@gmail.com>
2025-04-18 12:53:43 +0000

    MDX CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [
+    tailwind(), mdx(),
+  ]
 });
