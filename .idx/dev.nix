57d709f09e96ff8216ff2bf013cc4829d2c7d787
mahan.mattr <mahan.mattr@gmail.com>
2025-04-22 20:39:15 +0000

    Vibe coding into an insurmountable error log

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -26,8 +26,7 @@
       enable = true;
       previews = {
         web = {
-          command =
-            [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
+          command = [ "npm" "run" "start" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
